9d132731fe4700926855feb435ddca90a2c514f7
helinjiang <onehlj@qq.com>
2020-05-31 11:35:10 +0800

    feat: 04.mocha-opts

--- mocha/04.mocha-opts/test-use-mocha-opts/add.test.js
+++ mocha/04.mocha-opts/test-use-mocha-opts/add.test.js
@@ -1,7 +1,7 @@
 var add = require('../src/add.js');
 var expect = require('chai').expect;
 
-describe('加法函数的测试【test-in-other-dir】', function () {
+describe('[test-use-mocha-opts]加法函数的测试【test-in-other-dir】', function () {
     it('1 加 1 应该等于 2', function () {
         expect(add(1, 1)).to.be.equal(2);
     });
--- mocha/04.mocha-opts/test-use-mocha-opts/dir/multiply.test.js
+++ mocha/04.mocha-opts/test-use-mocha-opts/dir/multiply.test.js
@@ -1,7 +1,7 @@
 var multiply = require('../../src/multiply');
 var expect = require('chai').expect;
 
-describe('乘法函数的测试【test-in-other-dir】', function () {
+describe('[test-use-mocha-opts]乘法函数的测试【test-in-other-dir】', function () {
     it('1 乘 1 应该等于 1', function () {
         expect(multiply(1, 1)).to.be.equal(1);
     });
