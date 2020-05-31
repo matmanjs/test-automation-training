17eef6759f4846a2880a97f89cee73d2f8bc1747
helinjiang <onehlj@qq.com>
2020-05-31 16:08:56 +0800

    feat: 31.deal-absolute-paths

--- mocha/31.deal-absolute-paths/src/tools.js
+++ mocha/31.deal-absolute-paths/src/tools.js
@@ -1,5 +1,10 @@
 const util = require('./util');
-const config = require('./util/config');
+
+// 常规的写法
+// const config = require('./util/config');
+
+// 配置 resolve 之后的写法
+const config = require('util/config');
 
 function myTest(x, y) {
     return config.name + ': ' + x + '+' + y + '=' + util.add(x, y);
--- mocha/31.deal-absolute-paths/webpack.config.js
+++ mocha/31.deal-absolute-paths/webpack.config.js
@@ -4,6 +4,13 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist')
     },
+
+    // https://webpack.js.org/configuration/resolve/
+    resolve: {
+        alias: {
+            util: path.resolve(__dirname, 'src/util')
+        }
+    }
 };
