014a3ad29ac766ed96f084f00d40c9216eb01b5f
helinjiang <onehlj@qq.com>
2020-05-31 12:23:49 +0800

    feat: 05.es6

--- mocha/05.es6/test/add.test.js
+++ mocha/05.es6/test/add.test.js
@@ -1,10 +1,8 @@
+import { expect } from 'chai';
 import add from '../src/add.js';
-import chai from 'chai';
 
-let expect = chai.expect;
-
-describe('加法函数的测试', function () {
-    it('1 加 1 应该等于 2', function () {
+describe('加法函数的测试', () => {
+    it('1 加 1 应该等于 2', () => {
         expect(add(1, 1)).to.be.equal(2);
     });
 });
